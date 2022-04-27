b88306a1927255b1ad7abfa20fe00d9327e9f7b4
ammihir <48889548+ammihir@users.noreply.github.com>
2022-04-27 15:20:42 +0530

    starter for app

--- app.py
+++ app.py
@@ -1,64 +1,65 @@
 import streamlit as st
-from datetime import date
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+from  PIL import Image
+import numpy as np
+#import cv2
+import pandas as pd
+#from st_aggrid import AgGrid
+import plotly.express as px
+import io 
 
+#Below is MC##
+import streamlit as st
+import pandas as pd
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
-from plotly import graph_objs as go
+import numpy as np
+import pandas as pd
+import os
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+#import alpaca_trade_api as tradeapi
+####
+    
+    
+with st.sidebar:
+    choose = option_menu("Qauntics", ["Home", "Analysis", "Portfolio Engineering", "Forecasting", "Contact"],
+                         icons=['house', 'kanban', 'gear', 'activity','person lines fill'],
+                         menu_icon="quora", default_index=1,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
+    
+    
+logo = Image.open(r'test.jpg')
+profile = Image.open(r'test.jpg')
+if choose == "Home":
+    col1, col2 = st.columns( [0.8, 0.2])
+    with col1:               # To display the header text using css style
+        st.markdown(""" <style> .font {
+        font-size:35px ; font-family: 'Cooper Black'; color: #FF9633;} 
+        </style> """, unsafe_allow_html=True)
+        st.markdown('<p class="font">About the Creator</p>', unsafe_allow_html=True)    
+    with col2:               # To display brand log
+        st.image(logo, width=130 )
+    
+    st.write("Our objective to create a world class application to help beginners understand the naunces of investing in equity markets")    
+    st.image(profile, width=700 )
+    
+    
+elif choose == "Forecasting":
+    st.write("fORECASTING")
 
-START = "2015-01-01"
-TODAY = date.today().strftime("%Y-%m-%d")
-
-st.title('Stock Forecast App')
-
-stocks = ('GOOG', 'AAPL', 'MSFT', 'GME','idfc.ns')
-selected_stock = st.selectbox('Select dataset for prediction', stocks)
-
-n_years = st.slider('Years of prediction:', 1, 4)
-period = n_years * 365
-
-
-@st.cache
-def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace=True)
-    return data
-
-
-data_load_state = st.text('Loading data...')
-data = load_data(selected_stock)
-data_load_state.text('Loading data... done!')
-
-st.subheader('Raw data')
-st.write(data.tail())
-
-# Plot raw data
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
-
-plot_raw_data()
-
-# Predict forecast with Prophet.
-df_train = data[['Date','Close']]
-df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
-
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
-
-# Show and plot forecast
-st.subheader('Forecast data')
-st.write(forecast.tail())
-
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
-
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+    
+    
+elif choose == "Portfolio Engineering":
+    st.write("mCARLO")
+    
+elif choose == "Analysis":
+    st.write("analysis")
+    
+    
