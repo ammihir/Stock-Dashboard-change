ad80ed053ad336a68b8f6f5a1d32436057337bc8
ammihir <48889548+ammihir@users.noreply.github.com>
2022-04-27 16:37:48 +0530

    more changes

--- app.py
+++ app.py
@@ -52,6 +52,14 @@ if choose == "Home":
     
     
 elif choose == "Forecasting":
+    
+    from datetime import date
+
+    import yfinance as yf
+    from prophet import Prophet
+    from prophet.plot import plot_plotly
+    from plotly import graph_objs as go
+    
     START = "2015-01-01"
     TODAY = date.today().strftime("%Y-%m-%d")
 
